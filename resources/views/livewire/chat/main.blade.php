05386975d075a5c4dbec96b83f58a5c288ed2899
MFathirr <muhdyathir623@gmail.com>
2023-06-01 09:00:14 +0700

    prepos sass compiler

--- resources/views/livewire/chat/main.blade.php
+++ resources/views/livewire/chat/main.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Chat') }}
+        </h2>
+    </x-slot>
+</div>
+
+<div class="chat_container">
+    <div class="chat_list">
+        @livewire('chat.chatlist')
+    </div>
+    <div class="chat_box">
+        @livewire('chat.chatbox')
+        @livewire('chat.send-message')
+    </div>
 </div>
