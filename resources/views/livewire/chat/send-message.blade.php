06ba5329d4192cfc4d392782a9f28b9e383cdfa2
MFathirr <muhdyathir623@gmail.com>
2023-06-02 10:37:50 +0700

    send message design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,5 +1,8 @@
 <div>
     <div class="chatbox_footer">
-        footer new
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message">
+            <button class="submit" type="sumbit"><i class="bi bi-send-fill"></i></button>
+        </div>
     </div>
 </div>
