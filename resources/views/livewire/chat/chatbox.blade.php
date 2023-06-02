d9ffd8caa960ad11045e6f74c52b251ede050ed7
MFathirr <muhdyathir623@gmail.com>
2023-06-02 09:41:16 +0700

    design chatbox header

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,6 +1,19 @@
 <div>
     <div class="chatbox_header">
-        header
+        {{-- <div class="return">
+            <i class="bi bi-arrow-left"></i>
+        </div> --}}
+        <div class="img_container">
+            <img src="https://picsum.photos/200/300" alt="">
+        </div>
+        <div class="name">
+            Dya
+        </div>
+        <div class="info">
+            <div class="info_item"><i class="bi bi-telephone-fill"></i></div>
+            <div class="info_item"><i class="bi bi-image"></i></div>
+            <div class="info_item"><i class="bi bi-info-circle"></i></div>
+        </div>
     </div>
 
     <div class="chatbox_body">
